869beab2ca04e47b227ecae8c38cb8d00ea5aec2
shwetabh <shwetabh.srivastava@gmail.com>
2020-10-17 23:42:41 +0530

    shwetabhs

---
+++ Scenario2/src/generic/IAutoConst.java
@@ -0,0 +1,10 @@
+package generic;
+
+public interface IAutoConst {
+	String CHROME_KEY="webdriver.chrome.driver";
+	String CHROME_VALUE="./driver/chromedriver_78.exe";
+	
+	String XLPATH="./data/input.xlsx";
+}
+
+
---
+++ Scenario2/src/page/HomePage.java
@@ -0,0 +1,13 @@
+package page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+
+
+public class HomePage {
+
+	
+}
